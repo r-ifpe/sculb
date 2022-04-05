c8a2c7fef247c0156d44d7b9d4582a136b7b8fd3
samuelmacedo83 <svm.macedo@gmail.com>
2022-04-05 14:05:10 -0300

    sculb_ui

--- app.R
+++ app.R
@@ -1,97 +1,64 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(sculb)
 library(dplyr)
-library(ifpe.apps)
-library(xml2)
 
-ui <- fluidPage(
-    shiny::navbarPage(
-        "App Lattes",
-        tabPanel("Pontuação",
-                 sidebarLayout(
-                     sidebarPanel(
-                         shiny::fileInput(
-                             "lattes", "Escolha os curriculos lattes",
-                             multiple = TRUE,
-                             buttonLabel = "Arquivos",
-                             placeholder = "Nada Selecionado",
-                             accept = c("zip")
-                         )
-                     ),
-                     mainPanel(textOutput("lattes_output"))
-
-                 )
-                 )
-
-    )
-
-)
+ui <- sculb_ui()
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  output$lattes_output <- renderText({
+    if (is.null(input$lattes)) {
+      "Por favor insira os curriculos lattes"
+    } else {
+      output_folder <- utils::choose.dir()
+      t <- lapply(
+        input$lattes$datapath,
+        function(e) {
+          score_detalhado <- rbind(
+            lattes_titulacao(read_lattes_xml(e)),
+            lattes_bibliografia(read_lattes_xml(e)),
+            lattes_orientacoes(read_lattes_xml(e))
+          )
 
-   output$lattes_output <- renderText({
-       if (is.null(input$lattes)) {
-           "Por favor insira os curriculos lattes"
-       } else {
-           output_folder <- choose.dir()
-           t <- lapply(
-               input$lattes$datapath,
-               function(e) {
+          score <- data.frame(
+            nome = lattes_nome(read_lattes_xml(e)),
+            pontuacao_total = sum(score_detalhado$total)
+          )
 
-                   score_detalhado <- rbind(
-                       lattes_titulacao(read_lattes_xml(e)),
-                       lattes_bibliografia(read_lattes_xml(e)),
-                       lattes_orientacoes(read_lattes_xml(e))
-                   )
+          list(
+            nome = score$nome,
+            score = score,
+            score_detalhado = score_detalhado
+          )
+        }
+      )
 
-                   score <- data.frame(
-                       nome = lattes_nome(read_lattes_xml(e)),
-                       pontuacao_total = sum(score_detalhado$total)
-                   )
+      # escrever pontuacao dos professores
+      do.call(
+        "rbind",
+        lapply(1:length(t), function(e) {
+          t[[e]]$score
+        })
+      ) %>%
+        dplyr::arrange(desc(pontuacao_total)) %>%
+        openxlsx::write.xlsx(paste0(
+          output_folder, "/pontuacao_total.xlsx"
+        ))
 
-                   list(
-                       nome = score$nome,
-                       score = score,
-                       score_detalhado = score_detalhado
-                   )
-               }
-           )
-
-           # escrever pontuacao dos professores
-           do.call("rbind",
-                   lapply(1:length(t), function(e) {
-                       t[[e]]$score
-                   })
-           ) %>%
-               dplyr::arrange(desc(pontuacao_total)) %>%
-               openxlsx::write.xlsx(paste0(
-                 output_folder, "/pontuacao_total.xlsx"
-               )
-                   )
-
-           lapply(1:length(t), function(e) {
-               openxlsx::write.xlsx(
-                   t[[e]]$score_detalhado,
-                   paste0(output_folder,  "/",
-                          t[[e]]$nome, ".xlsx")
-               )
-           })
-       }
-
-
-     # "download realizado com sucesso!!!"
-   })
+      lapply(1:length(t), function(e) {
+        openxlsx::write.xlsx(
+          t[[e]]$score_detalhado,
+          paste0(
+            output_folder, "/",
+            t[[e]]$nome, ".xlsx"
+          )
+        )
+      })
+    }
 
 
+    # "download realizado com sucesso!!!"
+  })
 }
 
 # Run the application
